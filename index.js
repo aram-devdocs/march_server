1aaeb0bb9c3ff817ff63e595a17134ee37dcbdd7
Aram Hammoudeh <aramhammoudeh@Arams-MacBook-Pro.local>
2022-06-07 17:42:31 -0600

    login server setup

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const CLIENT_PORT = process.env.CLIENT_PORT;
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log(`Connected to ${process.env.MONGO_URI}`);
+  }
+)
+// .catch((e) => {
+// c
 
 // Allow CORS
 app.use(function (req, res, next) {
